e04141b4a1d03fc0c124ed0a432fe52c6c777f6c
CrystalCaveYoshi <louis.goerlt@gmail.com>
2026-06-01 16:53:29 +0200

    impl FormatError for CompilerError

--- ir-compiler/src/error/mod.rs
+++ ir-compiler/src/error/mod.rs
@@ -9,9 +9,11 @@ mod unparseable_constant;
 mod unprocessable_unit;
 mod value_not_dereferenceable;
 
+use colored::{Color, Colorize};
 pub use could_not_resolve_variable::CouldNotResolveVariable;
 pub use false_return_type::FalseReturnType;
 pub use incompatible_types::IncompatibleTypes;
+use std::ops::{Add, Range, RangeInclusive};
 pub use type_alias_has_different_size::TypeAliasHasDifferentSize;
 pub use unexpected_character::UnexpectedCharacterError;
 pub use unexpected_eof::UnexpectedEof;
@@ -34,4 +36,62 @@ pub enum CompilerError {
     ValueNotDereferenceable(ValueNotDereferenceable),
     TypeAliasHasDifferentSize(TypeAliasHasDifferentSize),
     CouldNotResolveVariable(CouldNotResolveVariable),
+}
+
+pub trait FormatError {
+    fn format(&self, src: &str) -> String;
+}
+
+impl FormatError for CompilerError {
+    fn format(&self, src: &str) -> String {
+        match self {
+            CompilerError::UnexpectedCharacterError(_) => todo!(),
+            CompilerError::UnexpectedTokenError(_) => todo!(),
+            CompilerError::UnexpectedEof(_) => todo!(),
+            CompilerError::UnparseableConstant(_) => todo!(),
+            CompilerError::IncompatibleTypes(_) => todo!(),
+            CompilerError::UnprocessableUnit(_) => todo!(),
+            CompilerError::FalseReturnType(_) => todo!(),
+            CompilerError::ValueNotDereferenceable(_) => todo!(),
+            CompilerError::TypeAliasHasDifferentSize(_) => todo!(),
+            CompilerError::CouldNotResolveVariable(_) => todo!(),
+        }
+    }
+}
+
+fn lines_within_range(
+    code: &str,
+    range: RangeInclusive<usize>,
+) -> Vec<(usize, &str, Range<usize>)> {
+    let mut offset = 0;
+    let mut result = Vec::new();
+    for (line_number, line) in code.split_inclusive('\n').enumerate() {
+        let length = line.len();
+        let line = line.trim_end();
+
+        let start = range.start().saturating_sub(offset);
+        let end = range.end().saturating_sub(offset + 1).min(line.len());
+
+        if start < end {
+            result.push((line_number, line, start..end));
+        }
+
+        offset += length;
+    }
+    result
+}
+
+fn highlight_lines(lines: &[(usize, &str, Range<usize>)]) -> String {
+    lines
+        .into_iter()
+        .map(|(line_num, line, Range { start, end })| {
+            format!(
+                "{0:0>5} {3} {line}\n      {3} {1}{2}\n",
+                line_num.add(1).to_string().color(Color::BrightBlue),
+                " ".repeat(*start),
+                "^".repeat(end - start).color(Color::Red),
+                "|".color(Color::BrightBlue),
+            )
+        })
+        .collect()
 }
