e35c7e2ab31472dc6f049b579cdfcaa7e4c9ad8a
Louis Goerlt <louis.goerlt@gmail.com>
2026-06-11 23:43:01 +0200

    way cooler example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 fn main() {
     let code = "
-fn swap(x: &u16, y: &u16) {
-    let temp = *x;
-    *x = *y;
-    *y = temp;
+fn color_at(x: u16, y: u16) -> u16 {
+    x * x + y * y
+}
+
+fn draw(getter: &fn(u16,u16) -> u16) {
+    device!(0);
+    context!(0, 1);
+    let x = 128 in for x downto 0 {
+        context!(1, x);
+        let y = 96 in for y downto 0 {
+            context!(2, y);
+            context!(3, getter(x, y));
+            send!();
+        };
+    };
+}
+
+fn flip() {
+    device!(0);
+    context!(0, 2);
+    send!();
 }
 
 fn main() {
-    let x = 6;
-    let y = 7;
-    swap(&x, &y);
-    dbg!(x, y);
+    let f = color_at;
+    loop {
+        draw(f);
+        flip();
+    }
 }
 ";
     let ir = ir_generator::compile(code).unwrap();
